ea0d870d2869032d3c15cf15d9abfff9e5da94ea
Hattyot <hattyot@gmail.com>
2019-02-01 16:58:55 +0200

    Update roleinfo.js

--- cmds/info/roleinfo.js
+++ cmds/info/roleinfo.js
@@ -13,6 +13,7 @@ module.exports.run = async (bot, message, args) => {
         })
     } else {
         let role = message.guild.roles.filter(role => role.name.toLowerCase() === roleName).first() || message.guild.roles.get(args[0]) || message.guild.roles.filter(role => role.name.toLowerCase().indexOf(roleName) != -1).first()
+        if(!role) return embedMaker.message(message, `I couldn't find that role`)
         let embed = new Discord.RichEmbed()
             .setColor(bot.config[message.guild.id].embedColor)
             .setAuthor(role.name, bot.icons[message.guild.id])
