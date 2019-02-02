f62fc1624874910016258c18176b12ce61ae2184
Hattyot <hattyot@gmail.com>
2019-02-02 21:08:22 +0200

    Update work.js

--- cmds/casino/work.js
+++ cmds/casino/work.js
@@ -35,6 +35,10 @@ module.exports.run = async (bot, message, args) => {
                 let min = 100;
                 return Math.floor(Math.random()*(max-min+1)+min)
             }else if (totalMoney < 15000) {
+                let max = 0.4 * totalMoney;
+                let min = 0.25 * totalMoney;
+                return Math.floor(Math.random()*(max-min+1)+min)
+            }else {
                 let max = 0.2 * totalMoney;
                 let min = 0.1 * totalMoney;
                 return Math.floor(Math.random()*(max-min+1)+min)
@@ -42,32 +46,28 @@ module.exports.run = async (bot, message, args) => {
         }
 
         function workSucessOrFail(addMoneyAmount) {
+            console.log(addMoneyAmount)
             let min = 3;
             let max = 10; // 30% chance of fail
             let chance = Math.floor(Math.random()*(max-min+1)+min);
+            let workCoolDownTime = 57600000; //16 hours
+
+            workCooldown.add(message.author.id);
+            bot.cooldownTimes.work[message.author.id] = Date.now() + workCoolDownTime;
+            setTimeout(() => {
+                workCooldown.delete(message.author.id)
+            }, workCoolDownTime);
 
             if(chance <= 3) {
                 money.findOneAndUpdate({user_ID: message.author.id}, {$inc: {inBank: -addMoneyAmount}}, {new: true}, (err, data) => {
                     if(err) return console.log(err)
+                    return embedMaker.message(message, `Your company is having a rough time and you lost **${currency}${addMoneyAmount}**`)
                 });
-                workCooldown.add(message.author.id);
-                let workCoolDownTime = 57600000; //16 hours
-                bot.cooldownTimes.work[message.author.id] = Date.now() + workCoolDownTime;
-                setTimeout(() => {
-                    workCooldown.delete(message.author.id)
-                }, workCoolDownTime);
-                return embedMaker.message(message, `Your company is having a rough time and you lost **${currency}${addMoneyAmount}**`)
             }else {
                 money.findOneAndUpdate({user_ID: message.author.id}, {$inc: {inBank: addMoneyAmount}}, {new: true}, (err, data) => {
                     if(err) return console.log(err)
+                    return embedMaker.message(message, `You had a good day at work and earned **${currency}${addMoneyAmount}**`)
                 });
-                workCooldown.add(message.author.id);
-                let workCoolDownTime = 57600000; //16 hours
-                bot.cooldownTimes.work[message.author.id] = Date.now() + workCoolDownTime;
-                setTimeout(() => {
-                    workCooldown.delete(message.author.id)
-                }, workCoolDownTime);
-                return embedMaker.message(message, `You had a good day at work and earned **${currency}${addMoneyAmount}**`)
             }
         }
     })
