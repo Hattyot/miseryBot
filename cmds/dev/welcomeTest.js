f953e2f14cc5cd8d91edc2346b569af7d1cb48c7
Hattyot <hattyot@gmail.com>
2019-02-03 17:49:39 +0200

    update

--- cmds/dev/welcomeTest.js
+++ cmds/dev/welcomeTest.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
 const Jimp = require("jimp");
-module.exports = async (bot, member) => {
-    let guild = member.guild
-    let welcomeChannel = "522979850651435013"
+module.exports.run = async (bot, message, args) => {
+    let guild = message.guild
+    let member = message.member
+    let welcomeChannel = "524064741543641108"
+    if (guild.id !== "522979850651435008") return;
     if (guild.id !== "522979850651435008") return;
     member.send(`Welcome to Misery's Box <@${member.id}>! Please read the rules in #rules-and-info. If you have any questions, feel free to ask a staff member`)
     Jimp.read("https://user-images.githubusercontent.com/39061940/52178067-699ca600-27d2-11e9-80bf-e5ceae9534fe.png").then((mainImage) => {
@@ -27,7 +29,7 @@ module.exports = async (bot, member) => {
                                 mainImage.print(font3, 90, 723, text3);
                                 mainImage.getBuffer(Jimp.MIME_PNG, (error, buffer) => {
                                     if (error) return console.log(error)
-                                    return guild.channels.get(welcomeChannel).send({
+                                    return guild.channels.get("536532064070270986").send({
                                         files: [{
                                             name: 'welcome.png',
                                             attachment: buffer
@@ -60,4 +62,17 @@ module.exports = async (bot, member) => {
         }
         return text3
     }
+};
+
+module.exports.help = {
+    name: "welcomeTest",
+    cat: "Dev",
+    description: "",
+    usage: "",
+    examples: [""]
+};
+
+module.exports.conf = {
+    enabled: true,
+    aliases: [],
 };
