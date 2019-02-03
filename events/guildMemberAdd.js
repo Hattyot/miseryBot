58ae8873e03a095cd20011cdcc646f335ef8ddd3
Hattyot <hattyot@gmail.com>
2019-02-03 14:07:49 +0200

    update

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const Jimp = require("jimp");
+exports.run = (client, member) => {
+    if (member.guild.id !== "522979850651435008") return;
+
+
+    Jimp.read("https://cdn.glitch.com/f95d51d0-e705-417b-a9c8-47bb29dd12cb%2Fwelcomer.png?1536011031764").then(function (image) {
+        Jimp.read(client.users.get(member.id).avatarURL).then(ima => {
+            ima.resize(420, 420);
+            Jimp.read('https://streamshark.io/obs-guide/img/image-mask.png').then(function (mask1) {
+                mask1.resize(420, 420);
+                ima.mask(mask1, 0, 0);
+                image.composite(ima, 152, 222);
+                Jimp.loadFont(Jimp.FONT_SANS_128_WHITE).then(function (font) {
+                    let text1 = "Welcome " + client.users.get(member.id).tag + "!";
+                    let text2 = "Now We Have: " + member.guild.memberCount + " Members.";
+                    image.print(font, 680, 280, text1);
+
+                    image.print(font, 680, 410, text2);
+                    image.write('welcome.png', () => {
+                        client.channels.get("479921142946332683").send({
+                            files: [
+                                "welcome.png"
+                            ]
+                        }).catch((err) => console.log(err));
+                    });
+                });
+            })
+        });
+    })
+};
