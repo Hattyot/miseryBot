8001231fcb7549c648004db837bd88fe4c30b4f5
Hattyot <hattyot@gmail.com>
2019-02-13 20:58:25 +0200

    update

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,24 @@
+const http = require("http")
+const fs = require('fs');
 module.exports = async (bot) => {
     console.log("Bot ready");
     bot.user.setActivity(`%help`)
+
+    http.createServer((req, res) => {
+        let responseCode = 404;
+        let content = '404 Error';
+
+        if (req.url === '/') {
+            responseCode = 200;
+            content = fs.readFileSync('../../index.html');
+        }
+
+        res.writeHead(responseCode, {
+            'content-type': 'text/html;charset=utf-8',
+        });
+
+        res.write(content);
+        res.end();
+    })
+        .listen(3000);
 };
