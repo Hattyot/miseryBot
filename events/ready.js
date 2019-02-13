f78c76bfbfea58349ef905f5c35bc35ff17cafea
Hattyot <hattyot@gmail.com>
2019-02-13 23:10:07 +0200

    update

--- events/ready.js
+++ events/ready.js
@@ -1,28 +1,4 @@
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
 module.exports = async (bot) => {
     console.log("Bot ready");
     bot.user.setActivity(`%help`)
-
-    const PORT = process.env.PORT || 3000;
-    const INDEX = path.join(__dirname, 'index.html');
-    console.log(INDEX)
-
-    const server = express()
-        .use((req, res) => res.sendFile(INDEX) )
-        .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-    const wss = new SocketServer({ server });
-
-    wss.on('connection', (ws) => {
-        console.log('Client connected');
-        ws.on('close', () => console.log('Client disconnected'));
-    });
-
-    setInterval(() => {
-        wss.clients.forEach((client) => {
-            client.send(new Date().toTimeString());
-        });
-    }, 1000);
 };
