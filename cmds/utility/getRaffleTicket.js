4a73d2994bcfb5a9a7d3e0dd7c27de127d382fe0
Hattyot <hattyot@gmail.com>
2019-02-13 22:39:59 +0200

    Update getRaffleTicket.js

--- cmds/utility/getRaffleTicket.js
+++ cmds/utility/getRaffleTicket.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
     raffle.findOne({user_ID: message.author.id}, (err, data) => {
         if(!data) {
-            let raffleID = (Math.round(Math.random() * 10000000) * Date.now())/5
+            let raffleID = (Math.round(Math.random() * 100000) * Date.now())/20
             let newRaffleTicket = new raffle({
                 user_ID: `${message.author.id}`,
                 raffle_ID: raffleID
@@ -14,6 +14,7 @@ module.exports.run = async (bot, message, args) => {
                 .catch(e => console.log(e));
             embedMaker.message(message, `You raffle ticket number is: **${raffleID}**`)
         }else {
+            let raffleID = data.raffle_ID
             embedMaker.message(message, `You already got a raffle ticket: **${raffleID}**`)
         }
     })
