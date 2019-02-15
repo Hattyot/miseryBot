9011da82f8fc6379c20767ddb37deffe7bd6f1b0
Hattyot <hattyot@gmail.com>
2019-02-15 21:21:01 +0200

    Create ratewaifu.js

---
+++ cmds/fun/ratewaifu.js
@@ -0,0 +1,25 @@
+let embedMaker = require("../../modules/embed.js")
+module.exports.run = async (bot, message, args) => {
+    let member = message.mentions.members.first()
+    if(!member) return embedMaker.command(message)
+    let content = `<@${member.user.id}>`.split("")
+    let sum = 0
+    for(let i = 0; i < content.length; i++) {
+        sum += Math.floor(content[i].charCodeAt())
+    }
+    let rating = sum % 101
+    return message.channel.send(`I rate ${member.user.id} with a ${rating}/100`)
+};
+
+module.exports.help = {
+    name: "ratewaifu",
+    cat: "Fun",
+    description: "See how good a waifu is",
+    usage: "ratewaifu [mention]",
+    examples: ["ratewaifu @HatMan"]
+};
+
+module.exports.conf = {
+    enabled: true,
+    aliases: [],
+};
