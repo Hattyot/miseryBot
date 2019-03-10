8d51fcfd1ef710345feea9e1ec5ff140a71de8f5
Hattyot <39061940+Hattyot@users.noreply.github.com>
2019-03-10 18:54:52 +0200

    Create voiceStateUpdate.js

---
+++ events/voiceStateUpdate.js
@@ -0,0 +1,15 @@
+module.exports = async (bot, oldMember, newMember) => {
+    if(newMember.guild.id === "522979850651435008") {
+        if(newMember.voiceChannelID) {
+            let role = newMember.guild.roles.get("554345816270307329")
+            newMember.addRole(role)
+        }else if(!newMember.voiceChannelID) {
+            let role = newMember.guild.roles.find("554345816270307329")
+            newMember.addRole(role)
+        }
+    }
+}
+module.exports.conf = {
+    enabled: true,
+    aliases: [],
+  };
