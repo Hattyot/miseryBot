f24713b6f42bf9239c913a3bf0b4ca144127b4b4
Hattyot <39061940+Hattyot@users.noreply.github.com>
2019-04-02 21:54:05 +0300

    Create bunny.js

---
+++ cmds/image/bunny.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const request = require('request');
+module.exports.run = async (bot, message, args) => {
+    request(`https://www.reddit.com/r/duck/.json?sort=rising&t=hour&limit=100`, (error, response, body) => {
+
+        if(error) return message.channel.send("slight error, whoops");
+        let json = JSON.parse(body);
+        let mainObject = json.data.children;
+        let urls = {};
+        let randomImageNumber = Math.floor(Math.random() * 80);
+
+        for(let i = 0; i < mainObject.length; i++) {
+            urls[i + 1] = mainObject[i].data.url;
+        }
+
+        let embed = new Discord.RichEmbed()
+            .setColor(bot.config[message.guild.id].colors.default)
+            .setImage(urls[randomImageNumber]);
+        message.channel.send(embed)
+    })
+};
+
+module.exports.help = {
+    name: "duck",
+    cat: "Image",
+    description: "Gives you a random duck image",
+    usage: "duck",
+    examples: ["duck"]
+};
+
+module.exports.conf = {
+    enabled: true,
+    aliases: [],
+};
