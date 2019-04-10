1493c8b534590c296567ce58e10f3d149bc7b3a7
Hattyot <39061940+Hattyot@users.noreply.github.com>
2019-04-10 09:43:34 +0300

    Update replaceColor.js

--- cmds/dev/replaceColor.js
+++ cmds/dev/replaceColor.js
@@ -1,22 +1,23 @@
 const Jimp = require('jimp');
 module.exports.run = async (bot, message, args) => {
-    let user = message.mentions.users.first();
-    if(!user) user = message.author
-
-    let linkRegex = /(https?:\/\/.*\.(?:png|jpg|gif|jpeg))/gi
-    let imageURL
-    linkRegex.exec(args[0]) ? imageURL = args[0] : imageURL = user.displayAvatarURL
-
-    Jimp.read(imageURL).then(function(image) {
-        Jimp.read("https://cdn.glitch.com/8c009d94-1f7e-464c-82c2-bccaf15cb6cd%2Fgay.png?1520010590279").then(function(image2) {
-            image.resize(768, 768);
-            image2.fade(0.6);
-            image.composite(image2, 0, 0);
-            image.getBuffer(Jimp.MIME_PNG, (error, buffer) => {
+    const replaceColor = require('replace-color')
+let targetColor = args[0]
+let replaceColor = args[1]
+replaceColor({
+  image: message.author.displayAvatarURL,
+  colors: {
+    type: 'hex',
+    targetColor: targetColor,
+    replaceColor: replaceColor
+  },
+  deltaE: 10
+})
+  .then((jimpObject) => {
+    jimpObject.getBuffer(Jimp.MIME_PNG, (error, buffer) => {
               message.channel.send({files: [{ name: 'gay.png', attachment: buffer }] });
             });
-      });
-    });
+  })
+
 }
 
 module.exports.help = {
